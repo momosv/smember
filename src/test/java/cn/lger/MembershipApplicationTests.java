334e5f85c34ef18fa5ef7621183ff6a8e1cd463e
linshengwen <momojy@vip.qq.com>
2018-12-24 18:27:39 +0800

    init

--- src/test/java/cn/lger/MembershipApplicationTests.java
+++ src/test/java/cn/lger/MembershipApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.lger;
 
+import cn.xsshome.taip.ocr.TAipOcr;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +13,33 @@ public class MembershipApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	//设置APPID/APP_KEY
+	public static final String APP_ID = "2109622308";
+	public static final String APP_KEY = "M25pVUiQFeSMWSxY";
+
+	public static void main(String[] args) throws Exception {
+		//https://blog.csdn.net/u010651369/article/details/80059442
+		String checkClientOption = "awd,";
+		checkClientOption = checkClientOption.substring(0,checkClientOption.length()-1);
+		System.out.println(checkClientOption);
+		// 初始化一个TAipOcr
+		TAipOcr aipOcr = new TAipOcr(APP_ID,APP_KEY);
+		// 调用接口
+//		String result = aipOcr.idcardOcr("./idcard.jpg", 0);//身份证正面(图片)识别
+//		String result = aipOcr.idcardOcr("./idcard2.jpg", 1);//身份证反面(国徽)识别
+//		String result = aipOcr.bcOcr("./juli2.jpg");//名片识别
+//		String result = aipOcr.driverlicenseOcr("./driver.jpg",0);//行驶证OCR识别
+//		String result = aipOcr.driverlicenseOcr("./driver2.jpg",1);//驾驶证OCR识别
+//		String result = aipOcr.bizlicenseOcr("./biz.jpg");//营业执照OCR识别
+//		String result = aipOcr.creditcardOcr("./bank2.jpg");//银行卡OCR识别
+//		String result = aipOcr.generalOcr("./biz.jpg");//通用OCR识别
+//		String result = aipOcr.handWritingOcrByImage("./biz.jpg");//手写体识别 选取本地图片文件识别
+//		String result = aipOcr.handWritingOcrByUrl("https://yyb.gtimg.com/ai/assets/ai-demo/small/hd-1-sm.jpg");//手写体识别 选取网络图片URL识别
+//		String result = aipOcr.plateOcrByImage("./biz.jpg");//车牌识别 选取本地图片文件识别
+//		String result = aipOcr.plateOcrByUrl("https://yyb.gtimg.com/ai/assets/ai-demo/large/plate-1-lg.jpg");//车牌识别 选取网络图片URL识别
+		String result = aipOcr.idcardOcr("E:\\Pictures\\odemo-pic-1.jpg", 0);//身份证正面(图片)识别
+		System.out.println(result);
+	}
+
 
 }
